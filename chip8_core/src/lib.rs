94da9220c8d88b4fa1ed9f0608458a3eea8b52bb
unknown <tsvetkovprelude@gmail.com>
2024-08-13 15:38:17 +0300

    basic instruction set and sdl2 window

--- chip8_core/src/lib.rs
+++ chip8_core/src/lib.rs
@@ -266,6 +266,84 @@ impl Emulator {
                     self.v_reg[0xF] = 0;
                 }
             },
+            (0xE, _, 9, 0xE)  => {        // SKIP if key press 
+                let x = digit2 as usize;
+                let vx = self.v_reg[x];
+                let key = self.keys[vx as usize];
+                if key {
+                    self.pc += 2;
+                }
+            },
+            (0xE, _, 0xA, 1)  => {        // SKIP if key not press 
+                let x = digit2 as usize;
+                let vx = self.v_reg[x];
+                let key = self.keys[vx as usize];
+                if !key {
+                    self.pc += 2;
+                }
+            },
+            (0xF, _, 0, 7)  => {        // VX = DT(delay timer)
+                let x = digit2 as usize;
+                self.v_reg[x] = self.dt;
+            },
+            (0xF, _, 0, 0xA)  => {      // WAIT KEY 
+                let x = digit2 as usize;
+                let mut pressed = false;
+                for i in 0..self.keys.len() {
+                    if self.keys[i] {
+                        self.v_reg[x] = i as u8;
+                        pressed = true;
+                        break;
+                   }
+                }
+
+                if !pressed {
+                    self.pc -= 2;
+                }
+            },
+            (0xF, _, 1, 5)  => {        // DT = VX 
+                let x = digit2 as usize;
+                self.dt = self.v_reg[x];
+            },
+            (0xF, _, 1, 8)  => {        // ST = VX 
+                let x = digit2 as usize;
+                self.st = self.v_reg[x];
+            },
+            (0xF, _, 1, 0xE)  => {        // I += VX 
+                let x = digit2 as usize;
+                let vx = self.v_reg[x] as u16;
+                self.i_reg = self.i_reg.wrapping_add(vx);
+            },
+            (0xF, _, 2, 9)  => {        // I = FONT 
+                let x = digit2 as usize;
+                let c = self.v_reg[x] as u16;
+                self.i_reg = c * 5;
+            },
+            (0xF, _, 3, 3)  => {        // binary-coded decimal (BCD) of a num in VX to ram;
+                let x = digit2 as usize;
+                let vx = self.v_reg[x] as f32;
+                let hundreds = (vx / 100.0).floor() as u8;
+                let tens = ((vx / 10.0) % 10.0).floor() as u8;
+                let ones = (vx % 10.0) as u8;
+
+                self.ram[self.i_reg as usize] = hundreds;
+                self.ram[(self.i_reg + 1) as usize] = tens;
+                self.ram[(self.i_reg + 2) as usize] = ones;
+            },
+            (0xF, _, 5, 5)  => {        // V0 -> VX store into RAM 
+                let x = digit2 as usize;
+                let i = self.i_reg as usize;
+                for idx in 0..=x {
+                    self.ram[i + idx] = self.v_reg[idx];
+                }
+            },
+            (0xF, _, 6, 5)  => {        // load V0 -> VX with some RAM stretch 
+                let x = digit2 as usize;
+                let i = self.i_reg as usize;
+                for idx in 0..=x {
+                    self.v_reg[idx] = self.ram[i + idx];
+                }
+            },
             (_, _, _, _) => unimplemented!("Unimplemented opcode: {}", op),
         }
     }
@@ -290,6 +368,20 @@ impl Emulator {
         let op = (higher_byte << 8) | lower_byte;
         self.pc += 2;
         op
+    }
+
+    pub fn get_display(&self) -> &[bool] {
+        &self.screen
+    }
+
+    pub fn keypress(&mut self, idx: usize, pressed: bool) {
+        self.keys[idx] = pressed;
+    }
+
+    pub fn load(&mut self, data: &[u8]) {
+        let start = START_ADDR as usize;
+        let end = (START_ADDR as usize) + data.len();
+        self.ram[start..end].copy_from_slice(data);
     }
 
 }
